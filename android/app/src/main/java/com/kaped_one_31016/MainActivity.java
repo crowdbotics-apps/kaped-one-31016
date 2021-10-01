dd598d8dc06de1e010fab0b6250fc307809d8d61
Crowdbotics <team@crowdbotics.com>
2021-10-01 07:21:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/kaped_one_31016/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kaped_one_31016;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "kaped_one_31016";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "kaped_one_31016",
+  url: "https://kaped_one_31016.botics.co",
+  api: "https://kaped_one_31016.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
